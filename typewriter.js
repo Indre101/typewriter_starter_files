2d3bc772b3a03a988dede6b1760443ea9db823ac
indre101 <zygaityte.indre@gmail.com>
2020-02-10 18:43:06 +0100

    function to clear interval

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,30 @@
 "use strict";
+
+const typewritten = document.querySelector(".typewritten");
+let sentence = typewritten.textContent
+
+const findTheLenghtOfSentence = sentence => sentence.length
+
+const randomTimeOffset = () => Math.random() * 1000
+
+let i = 0;
+
+const showNextLetter = (sentence) => {
+  typewritten.textContent = sentence.substring(0, i)
+}
+
+
+
+let nextLetterInteval = setInterval(() => {
+  i++
+  showNextLetter(sentence)
+  stopInterval(i)
+  console.log(i);
+
+}, 1000);
+
+function stopInterval(i) {
+  if (i == findTheLenghtOfSentence(sentence)) {
+    clearInterval(nextLetterInteval)
+  }
+}
