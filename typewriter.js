11134311bed8c27116895c2f6a6254648fa2ad43
indre101 <zygaityte.indre@gmail.com>
2020-02-11 10:53:13 +0100

    changed to one interval

--- typewriter.js
+++ typewriter.js
@@ -1,33 +1,37 @@
 "use strict";
 
+const audio = new Audio('typekey1.mp3');
+
 const typewritten = document.querySelector(".typewritten");
 let sentence = typewritten.textContent
+typewritten.textContent = " "
 
 const findTheLenghtOfSentence = sentence => sentence.length
-const randomTimeOffset = () => 4000 - (Math.random() * 2000)
+const randomTimeOffset = () => Math.floor(Math.random() * 500) + 400;
 
-let randomDelay = randomTimeOffset();
 let i = 0;
-
-
+let randomDelay = randomTimeOffset();
 
 const getNextLetter = (sentence) => {
-  typewritten.textContent = sentence.substring(0, i)
+  sentence = sentence.charAt(i);
+  return sentence;
 }
 
 
-let addNextLetter = () => {
-  const intervalOne = setTimeout(() => {
-    i++
-    console.log("object");
-    getNextLetter(sentence)
+
+let newSentence = getNextLetter(sentence)
+
+function typeWritte() {
+  const intervalOne = setInterval(() => {
     randomDelay = randomTimeOffset();
-
-    if (i == findTheLenghtOfSentence(sentence)) {
-      clearInterval(printSentence);
+    newSentence = getNextLetter(sentence);
+    if (i <= sentence.length) {
+      typewritten.textContent = typewritten.textContent + newSentence;
+      i++
+      clearInterval(intervalOne);
+      typeWritte()
     }
-
   }, randomDelay);
 }
 
-const printSentence = setInterval(addNextLetter, 500);
+typeWritte();
