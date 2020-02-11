e7d75b40155928567dce06fb158c8ba1fd3a8c36
indre101 <zygaityte.indre@gmail.com>
2020-02-11 15:57:20 +0100

    typewrittes multiple sentences

--- typewriter.js
+++ typewriter.js
@@ -4,35 +4,48 @@ const typeKeyOne = new Audio('typekey1.mp3');
 const typeKeyTwo = new Audio('typekey2.mp3');
 const typeKeySpace = new Audio('typespace.mp3');
 const typeKeyEndLine = new Audio('typelastkey.mp3');
-
 const audioSounds = [typeKeyOne, typeKeyTwo, typeKeySpace, typeKeyEndLine]
 
 const typewritten = document.querySelectorAll(".typewritten");
-const randomTimeOffset = () => Math.floor(Math.random() * 500) + 100;
+const randomTimeOffset = () => Math.floor(Math.random() * 200) + 200;
 let randomDelay = randomTimeOffset();
 
-typewritten.forEach(typewrittenSentence => {
-  let sentence = typewrittenSentence.textContent
-  typewrittenSentence.textContent = " "
-  let counter = 0;
-  typeWritte(sentence, typewrittenSentence, counter);
+const sentences = [];
 
+typewritten.forEach(typewrittenSentence => {
+  typewrittenSentence.setAttribute("data-sentence", typewrittenSentence.textContent)
+  typewrittenSentence.textContent = " "
+  sentences.push(typewrittenSentence.dataset.sentence);
 })
 
 
-function typeWritte(sentence, typewrittenSentence, counter) {
+let index = 0;
+printSentences(index)
+
+function printSentences(index) {
+  let counter = 0;
+  if (index < sentences.length) {
+    typeWritte(sentences[index], typewritten[index], counter);
+  } else {
+    return false;
+  }
+}
+
+
+function typeWritte(sentence, placetoPrintTheText, counter) {
   randomDelay = randomTimeOffset();
-
-
-
   setTimeout(() => {
-    if (counter <= sentence.length) {
-      addSounds(getNextLetter(counter, sentence), counter)
-      typewrittenSentence.textContent += getNextLetter(counter, sentence);
+    if (counter < sentence.length) {
+      addSounds(getNextLetter(counter, sentence));
+      placetoPrintTheText.textContent += getNextLetter(counter, sentence);
       counter++
-      typeWritte(sentence, typewrittenSentence, counter)
+      typeWritte(sentence, placetoPrintTheText, counter)
     } else {
       audioSounds[3].play();
+      // Start new sentence
+      index++
+      counter = 0;
+      printSentences(index)
     }
   }, randomDelay);
 }
@@ -42,8 +55,7 @@ function getNextLetter(counter, sentence) {
   return sentence.charAt(counter);
 }
 
-function addSounds(character, counter) {
-
+function addSounds(character) {
   if (character != " ") {
     audioSounds[Math.floor(Math.random() * 2)].play();
   } else if (character == " ") {
