e12d5e547600434c39a3236caa3f273524b46612
indre101 <zygaityte.indre@gmail.com>
2020-02-11 18:54:56 +0100

    Adds correct sounds

--- typewriter.js
+++ typewriter.js
@@ -19,8 +19,9 @@ typewritten.forEach(typewrittenSentence => {
 })
 
 
+
 let index = 0;
-printSentences(index)
+
 
 function printSentences(index) {
   let counter = 0;
@@ -33,21 +34,25 @@ function printSentences(index) {
 
 
 function typeWritte(sentence, placetoPrintTheText, counter) {
-  randomDelay = randomTimeOffset();
   setTimeout(() => {
-    if (counter < sentence.length) {
-      addSounds(getNextLetter(counter, sentence));
+    addSounds(getNextLetter(counter, sentence), sentence, counter);
+
+    if (counter < sentence.length - 1) {
       placetoPrintTheText.textContent += getNextLetter(counter, sentence);
       counter++
       typeWritte(sentence, placetoPrintTheText, counter)
+
     } else {
-      audioSounds[3].play();
+      console.log("end");
       // Start new sentence
       index++
       counter = 0;
       printSentences(index)
     }
   }, randomDelay);
+
+  randomDelay = randomTimeOffset();
+
 }
 
 
@@ -55,10 +60,36 @@ function getNextLetter(counter, sentence) {
   return sentence.charAt(counter);
 }
 
-function addSounds(character) {
-  if (character != " ") {
+
+
+function addSounds(character, sentence, counter) {
+  // console.log(sentence.indexOf(character), character);
+  if (counter == sentence.length - 1) {
+    audioSounds[3].play();
+    randomDelay += 5000;
+  } else if (character != " ") {
     audioSounds[Math.floor(Math.random() * 2)].play();
   } else if (character == " ") {
     audioSounds[2].play();
+  }
+}
+
+let soundStatus = false;
+silenceSounds(soundStatus);
+
+document.querySelector("button").onclick = function () {
+  printSentences(index)
+  // if (soundStatus = true) {
+
+  // }
+}
+
+function silenceSounds(status) {
+  if (!status) {
+    audioSounds.forEach(audios => audios.pause());
+
+  } else {
+    audioSounds.forEach(audios => audios.play());
+
   }
 }
