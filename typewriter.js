3744f673871d709cb257a82237401d19d6d35672
indre101 <zygaityte.indre@gmail.com>
2020-02-11 14:42:35 +0100

    adds sound

--- typewriter.js
+++ typewriter.js
@@ -1,6 +1,11 @@
 "use strict";
 
-const audio = new Audio('typekey1.mp3');
+const typeKeyOne = new Audio('typekey1.mp3');
+const typeKeyTwo = new Audio('typekey2.mp3');
+const typeKeySpace = new Audio('typespace.mp3');
+const typeKeyEndLine = new Audio('typelastkey.mp3');
+
+const audioSounds = [typeKeyOne, typeKeyTwo, typeKeySpace, typeKeyEndLine]
 
 const typewritten = document.querySelectorAll(".typewritten");
 const randomTimeOffset = () => Math.floor(Math.random() * 500) + 100;
@@ -10,20 +15,38 @@ typewritten.forEach(typewrittenSentence => {
   let sentence = typewrittenSentence.textContent
   typewrittenSentence.textContent = " "
   let counter = 0;
-
   typeWritte(sentence, typewrittenSentence, counter);
-
 
 })
 
 
 function typeWritte(sentence, typewrittenSentence, counter) {
   randomDelay = randomTimeOffset();
+
+
+
   setTimeout(() => {
     if (counter <= sentence.length) {
-      typewrittenSentence.textContent += sentence.charAt(counter);
+      addSounds(getNextLetter(counter, sentence), counter)
+      typewrittenSentence.textContent += getNextLetter(counter, sentence);
       counter++
       typeWritte(sentence, typewrittenSentence, counter)
+    } else {
+      audioSounds[3].play();
     }
   }, randomDelay);
+}
+
+
+function getNextLetter(counter, sentence) {
+  return sentence.charAt(counter);
+}
+
+function addSounds(character, counter) {
+
+  if (character != " ") {
+    audioSounds[Math.floor(Math.random() * 2)].play();
+  } else if (character == " ") {
+    audioSounds[2].play();
+  }
 }
